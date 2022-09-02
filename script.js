98bb35cab28320e7b267032ce229c4a87f2a8943
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-09-02 18:39:31 +0100

    end function and time variables

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+let score = 0;
+let timeLeft = 0;
+let timer;
+
+
 function start() {
-    timeLeft = 100;
+    timeLeft = 5;
     document.getElementById("timeLeft").innerHTML = timeLeft;
 
     timer = setInterval(function () {
@@ -13,4 +18,25 @@ function start() {
     }, 1000);
 
     next();
+}
+
+function endGame() {
+  clearInterval(timer);
+
+  let quizContent =
+    ` <h2>Game over</h2>
+
+<h3>You got a ` +
+    score +
+    ` /100!</h3>
+
+<h3>You got ` +
+    score / 20 +
+    ` questions correct</h3>
+
+<input type="text" id="name" placeholder="First name"> 
+
+<button onclick="setScore()">Set score</button>`;
+
+  document.getElementById("quizBody").innerHTML = quizContent;
   }
